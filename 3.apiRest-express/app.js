8d68d35c24368f525febbd1e9d9ce67c6ef903ec
GonzaBolognese <gonza.1703@gmail.com>
2024-01-12 14:02:07 -0300

    Endpoints /Movies and /Movies:id created also /Movies filters genres by query

---
+++ 3.apiRest-express/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const moviesJSON = require('./movies.json')
+
+const app = express()
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hola Mundo' })
+})
+
+app.get('/movies', (req, res) => {
+  if (Object.keys(req.query).length === 0) {
+    res.status(200).json(moviesJSON)
+  } else {
+    const { genre } = req.query
+    const listGenre = moviesJSON.filter((m) => m.genre.includes(genre))
+    res.status(200).json(listGenre)
+  }
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  res.status(200).json(moviesJSON[id - 1])
+})
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}`)
+})
