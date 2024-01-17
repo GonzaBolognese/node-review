0b83133bf0a133c9bf1e91a7692d93240b3fc012
GonzaBolognese <gonza.1703@gmail.com>
2024-01-17 13:56:29 -0300

    Reorganizacion de rutas: Implemente la arquitectura MVC para poder dividir el modelo de negocio en models, routes y view

--- 4.arquitectura-mvc/app.js
+++ 4.arquitectura-mvc/app.js
@@ -1,28 +1,11 @@
 import express, { json } from 'express'
-import cors from 'cors'
 import moviesRouter from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 app.disable('x-powered-by')
 app.use(json())
-app.use(cors({
-  origin: (origin, callback) => {
-    const ACCEPTED_ORIGINS = [
-      'http://localhost:8080',
-      'http://localhost:3000',
-      'https://gonzalo-bolognese.vercel.app/'
-    ]
-    if (ACCEPTED_ORIGINS.includes(origin)) {
-      return callback(null, true)
-    }
-
-    if (!origin) {
-      return callback(null, true)
-    }
-
-    return callback(new Error('Not allowed by CORS'))
-  }
-}))
+app.use(corsMiddleware())
 
 app.use('/movies', moviesRouter)
 
